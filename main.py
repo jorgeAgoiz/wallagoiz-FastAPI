fdafe31a757a6f30099cf1ba3c9db6fe2c457851
jorgeAP <jorgeagoiz@gmail.com>
2021-12-30 08:23:11 +0100

    Learning FastAPI

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    email: str
+    password: str
+
 
 app = FastAPI()
 # Comienzo a montar el servidor
 
 
-@app.get('/')
+@app.get('/signup')
 async def root():
     return {'message': 'Hello World!!'}
+
+
+@app.post('/signin')
+async def signin(user: User):
+    print(user)
+    return {'data': user}
