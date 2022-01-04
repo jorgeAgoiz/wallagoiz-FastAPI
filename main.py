f3593db28852024f52b7725fe1c5e3b5786550cb
jorgeAP <jorgeagoiz@gmail.com>
2022-01-04 18:53:49 +0100

    Still learning about FastAPI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from random import randrange
 from typing import Optional
 from pydantic import BaseModel
 
@@ -8,16 +9,33 @@ class User(BaseModel):
     password: str
 
 
+# Variable to test my firsts endpoints
+users = [{"id": 1, "email": "probando@gmail.com", "password": "loquesea"},
+         {"id": 2, "email": "otraprueba@gmail.com", "password": "loqueseaversiondos"}]
+
 app = FastAPI()
 # Comienzo a montar el servidor
 
 
-@app.get('/signup')
-async def root():
-    return {'message': 'Hello World!!'}
+@app.get('/users')
+async def get_users():
+    return users
 
 
-@app.post('/signin')
-async def signin(user: User):
-    print(user)
-    return {'data': user}
+@app.get('/users/{id}')
+async def get_user_with(id: int):
+    for user in users:
+        print(user)
+        if user['id'] == id:
+            return {'Your user': user}
+        else:
+            return {'message': 'User not found'}
+
+
+@app.post('/users')
+async def create_user(user: User):
+    user_new = user.dict()
+    user_new['id'] = randrange(0, 1000)
+    print(user_new)
+    users.append(user_new)
+    return {'New user': user_new, 'All users': users}
