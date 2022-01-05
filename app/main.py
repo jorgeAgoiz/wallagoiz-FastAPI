7f69351b8152bda0c8ef4ffce32a928d22199c78
jorgeAP <jorgeagoiz@gmail.com>
2022-01-05 10:06:33 +0100

    Improving with fastAPI

--- app/main.py
+++ app/main.py
@@ -1,42 +1,6 @@
-from fastapi import FastAPI, Response
-from random import randrange
-from typing import Optional
-from pydantic import BaseModel
-
-
-class User(BaseModel):
-    email: str
-    password: str
-
-
-# Variable to test my firsts endpoints
-users = [{"id": 1, "email": "probando@gmail.com", "password": "loquesea"},
-         {"id": 2, "email": "otraprueba@gmail.com", "password": "loqueseaversiondos"}]
+from fastapi import FastAPI
+from routers.users import user
 
 app = FastAPI()
-# Comienzo a montar el servidor
 
-
-@app.get('/users')
-async def get_users():
-    return users
-
-
-@app.get('/users/{id}')
-async def get_user_with(id: int, response: Response):
-    for user in users:
-        print(user)
-        if user['id'] == id:
-            return {'Your user': user}
-        else:
-            response.status_code = 404
-            return {'message': 'User not found'}
-
-
-@app.post('/users')
-async def create_user(user: User):
-    user_new = user.dict()
-    user_new['id'] = randrange(0, 1000)
-    print(user_new)
-    users.append(user_new)
-    return {'New user': user_new, 'All users': users}
+app.include_router(user)
---
+++ app/routers/users.py
@@ -0,0 +1,61 @@
+from fastapi import APIRouter, Response, status
+from random import randrange
+from fastapi.exceptions import HTTPException
+from pydantic import BaseModel
+
+user = APIRouter()
+
+
+class User(BaseModel):
+    email: str
+    password: str
+
+
+# Variable to test my firsts endpoints
+users = [{"id": 1, "email": "probando@gmail.com", "password": "loquesea"},
+         {"id": 2, "email": "otraprueba@gmail.com",
+             "password": "loqueseaversiondos"},
+         {"id": 3, "email": "yanoteacuerdas@gmail.com", "password": "loqueseaversiontres"}]
+
+
+def find_index_user(id):
+    for i, u in enumerate(users):
+        if(u['id'] == id):
+            print(i)
+            return i
+
+
+@user.get('/users')
+async def get_users():
+    return users
+
+
+@user.get('/users/{id}')
+async def get_user_with(id: int, response: Response):
+    for user in users:
+        print(user)
+        if user['id'] == id:
+            return {'Your user': user}
+        else:
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return {'message': 'User not found'}
+
+
+@user.post('/users', status_code=status.HTTP_201_CREATED)
+async def create_user(user: User):
+    user_new = user.dict()
+    user_new['id'] = randrange(0, 1000)
+    print(user_new)
+    users.append(user_new)
+    return {'New user': user_new, 'All users': users}
+
+
+@user.delete('/users/{id}')
+async def delete_user(id: int, response: Response):
+    user_index = find_index_user(id)
+    if user_index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'User with {id} ID, was not found.')
+    else:
+        users.pop(user_index)
+        return {"Users": users}
