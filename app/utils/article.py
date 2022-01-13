90fc3309c585d1b23838ec054f6ad44bad4390f8
jorgeAP <jorgeagoiz@gmail.com>
2022-01-13 15:06:14 +0100

    Refactoring some routes about articles

--- app/utils/article.py
+++ app/utils/article.py
@@ -18,8 +18,16 @@ def get_articles(db: Session):
     return db.query(ArticleDB).all()
 
 
-def get_article_by(id: int, db: Session):
+def get_article_by_id(id: int, db: Session):
     return db.query(ArticleDB).filter_by(id=id).first()
+
+
+def get_article_by_userId(user_id: int, db: Session):
+    return db.query(ArticleDB).filter_by(userId=user_id).all()
+
+
+def get_article_by_category(category: str, db: Session):
+    return db.query(ArticleDB).filter_by(category=category).all()
 
 
 def remove_article(id: int, user_id: int, db: Session):
